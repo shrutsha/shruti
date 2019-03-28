58170199794ea078ab52452b34e74515c40d3766
Shruti Sharma <shruti.sharma@non.keysight.com>
2019-03-28 12:35:56 +0530

    git_jenkins

---
+++ git_jenkins/TestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using git_jenkins;
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Assert.AreEqual("Welcome to Jenkins", Program.createMessage());
+        }
+
+    }
+}
---
+++ git_jenkins/git_jenkins/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace git_jenkins
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(createMessage());
+        }
+        public static String createMessage()
+        {
+            return "Welcome to Jenkins";
+        }
+    }
+}
